11e5caf5c5d63312bfed7ada3dcfe4badf92d1d1
SilverSTV <Silverstv@yandex.ru>
2026-05-23 10:09:20 +0300

    Инкапсулировал настройки ресурсов

--- Assets/Game/Scripts/Gameplay/Resources/ResourceDefinition.cs
+++ Assets/Game/Scripts/Gameplay/Resources/ResourceDefinition.cs
@@ -6,8 +6,14 @@ namespace Game.Scripts.Gameplay.PlayerResources
     [Serializable]
     public sealed class ResourceDefinition
     {
-        public ResourceType ResourceType;
-        public string DisplayName;
-        public Sprite Icon;
+        private ResourceType _resourceType;
+        private string _displayName;
+        private Sprite _icon;
+
+        public ResourceType ResourceType => _resourceType;
+
+        public string DisplayName => _displayName;
+
+        public Sprite Icon => _icon;
     }
 }
